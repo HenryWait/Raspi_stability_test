a269ecca4efd4e7e6d137210443277b802d13c9d
HWWAIT <HenryWait@users.noreply.github.com>
2018-08-06 23:36:57 +1000

    Numerous chagnes and additions
    Add ASCII art header
    Add Menu 
    Change stress_test function so that the number of threads is set to the number of cores, now script works on all Raspi models

--- stability_test.sh
+++ stability_test.sh
@@ -4,6 +4,19 @@
 
 # clear the screen
 clear
+# print ascii art header
+echo "$(tput setaf 2)
+   .~~.   .~~.
+  '. \ ' ' / .'$(tput setaf 1)
+   .~ .~~~..~.
+  : .~.'~'.~. :
+ ~ (   ) (   ) ~
+( : '~'.~.'~' : )
+ ~ .~ (   ) ~. ~
+  (  : '~' :  ) $(tput sgr0) Stability Test$(tput setaf 1)
+   '~ .~~~. ~'
+       '~'
+$(tput sgr0)" 
 
 #show CPU frequency and temperature function:
 temp_freq () {
@@ -11,11 +24,35 @@ temp_freq () {
     vcgencmd measure_temp
 }
 
-#show CPU frequency and temperature before running at full load:
-temp_freq
+stress_test () {
+    #set variable to number of cores the system has
+    local CORES=nproc
+    #sysbench test, runs pi at full tilt for long enough to reach max temp
+    sysbench --test=cpu --cpu-max-prime=50000 --num-threads=$CORES run >/dev/null 2>&1
+}
 
-#sysbench test
-sysbench --test=cpu --cpu-max-prime=50000 --num-threads=4 run >/dev/null 2>&1
-
-#show CPU frequency and temperature after running at full load:
-temp_freq
+# Menu 
+PS3='Please enter your choice: '
+options=("Stress Test" "Show temperature" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Stress Test")
+            echo "Running sysbench"
+            #show CPU frequency and temperature before running at full load:
+            temp_freq
+            stress_test
+            #show CPU frequency and temperature after running at full load:
+            temp_freq
+            ;;
+        "Show temperature")
+            echo ""
+            #show CPU frequency and temperature
+            temp_freq
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
